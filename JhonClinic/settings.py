2cda29b6aebdd0a432c7ea6aa88b20ff99dcfe2d
Rizni <mnriznimohamed@gmail.com>
2022-08-12 22:21:28 +0530

    ifx

--- JhonClinic/settings.py
+++ JhonClinic/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static_build"]
 STATIC_URL = "static"
 
