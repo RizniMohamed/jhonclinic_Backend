eab9a1d862c2d28ba006f77fb0af95037fe9ba93
Rizni <mnriznimohamed@gmail.com>
2022-08-12 17:32:22 +0530

    fix

--- JhonClinic/settings.py
+++ JhonClinic/settings.py
@@ -144,8 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn JhonClinic.wsgi
+config:set DISABLE_COLLECTSTATIC=1
