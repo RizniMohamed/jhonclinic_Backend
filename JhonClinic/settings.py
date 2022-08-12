6d52366b2cdec34bce43b5eaa7dc0dec3e41cc40
Rizni <mnriznimohamed@gmail.com>
2022-08-12 16:26:02 +0530

    fix

--- JhonClinic/settings.py
+++ JhonClinic/settings.py
@@ -63,8 +63,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -72,7 +71,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'JhonClinic.urls'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
