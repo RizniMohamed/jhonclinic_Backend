81fd35dd4b303e0c7c95fdb69cf245ec6453f23d
Rizni <mnriznimohamed@gmail.com>
2022-08-12 23:35:36 +0530

    fix

--- JhonClinic/settings.py
+++ JhonClinic/settings.py
@@ -141,8 +141,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
