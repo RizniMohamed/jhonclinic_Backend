e5519d79080f7464f4f1edaf97a63e9258d67d1b
Rizni <mnriznimohamed@gmail.com>
2022-08-12 22:19:13 +0530

    fix

--- JhonClinic/settings.py
+++ JhonClinic/settings.py
@@ -151,7 +151,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "static_build"]
-
+STATIC_URL = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
